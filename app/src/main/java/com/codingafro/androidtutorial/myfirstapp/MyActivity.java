e3c0f7b2af9eb98948fe8f24c29ef6965ae3be17
Patrick Kayongo <pkayongo@openboxsoftware.com>
2015-03-23 16:55:48 +0200

    First commit

---
+++ app/src/main/java/com/codingafro/androidtutorial/myfirstapp/MyActivity.java
@@ -0,0 +1,65 @@
+package com.codingafro.androidtutorial.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MyActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "com.codingafro.androidtutorial.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        MenuInflater inflater = this.getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                this.openSearch();
+                return true;
+            case R.id.action_settings:
+                this.openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)this.findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        this.startActivity(intent);
+
+    }
+
+    private void openSearch() {
+    }
+
+    private void openSettings() {
+    }
+}
